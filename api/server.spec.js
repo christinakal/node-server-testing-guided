634adaf92b4958a26da073b32901d9dad760a5c2
christina kaliora <develop@pirron.one>
2020-03-26 10:51:37 +0100

    another test is running

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,6 +1,19 @@
-const server = require('./server');
+const server = require("./server");
+const request = require("supertest");
 
-it('is running with the correct db', () => {
-    expect(process.env.DB_ENV).toBe('testing');
-})
+it("is running with the correct db", () => {
+  expect(process.env.DB_ENV).toBe("testing");
+});
 
+describe("server.js", () => {
+  describe("GET on /", () => {
+    it("runs without errors", () => {
+      return request(server)
+        .get("/")
+        .expect(200)
+        .expect("Content-Type", /json/);
+    });
+  });
+
+  describe("GET on /hobbits", () => {});
+});
