91706c71b1d97e51c8721c7e150edd676188395e
christina kaliora <develop@pirron.one>
2020-03-26 10:42:41 +0100

    set up 2 tests running

---
+++ api/server.spec.js
@@ -0,0 +1,6 @@
+const server = require('./server');
+
+it('is running with the correct db', () => {
+    expect(process.env.DB_ENV).toBe('testing');
+})
+
--- data/dbConfig.js
+++ data/dbConfig.js
@@ -4,3 +4,4 @@ const config = require('../knexfile.js');
 const environment = process.env.DB_ENV || 'development';
 
 module.exports = knex(config[environment]);
+ 
